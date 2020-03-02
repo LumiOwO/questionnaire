6810e9c8a91aedc49cba68f999b8e8636b9cdca0
SolarBear251 <948697775@qq.com>
2020-03-02 20:04:12 +0800

    Initial commit

---
+++ src/main/java/com/example/questionnaire/MainApplication.java
@@ -0,0 +1,20 @@
+package com.example.questionnaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MainApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MainApplication.class);
+	}
+
+}
---
+++ src/main/java/com/example/questionnaire/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.example.questionnaire.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	@RequestMapping("/")
+	public String index() {
+		return "hello world!";
+	}
+}
